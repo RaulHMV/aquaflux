423601968aa6b690bcaf9dc1581991ed44d1bee1
RaulHMV <23160067@uttcampus.edu.mx>
2025-10-29 01:28:42 -0600

    fix: remove transpilePackages conflict with serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Solo le decimos a Next.js que estos paquetes son externos al componente de servidor
-  // pero SÍ deben incluirse en el bundle de Vercel
   serverExternalPackages: ['pg', 'pg-hstore', 'sequelize'],
-  
-
-  
-  // Opcional: Si tienes problemas con ESM
-  transpilePackages: ['sequelize'],
 };
 
 export default withFlowbiteReact(nextConfig);
